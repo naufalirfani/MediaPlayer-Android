ffc4c58478e57441c2ceae8bf4de64839dd66a7c
naufalirfani <naufal.irfani3@gmail.com>
2020-10-02 17:25:05 +0700

    initial commit

--- app/src/main/java/com/example/videoviewapp/MainActivity.kt
+++ app/src/main/java/com/example/videoviewapp/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.videoviewapp
 
-import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.view.Window
 import android.view.WindowManager
 import android.widget.MediaController
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
-import kotlinx.android.synthetic.main.activity_main.*
-import javax.sql.DataSource
+import kotlinx.android.synthetic.main.exo_player_control_view.*
 
 
 @Suppress("DEPRECATION")
@@ -27,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var simpleExoPlayer: SimpleExoPlayer
     private val url = "https://firebasestorage.googleapis.com/v0/b/simplecrud-bb3c3.appspot.com/o/Captures%202020-03-31%2013-32-47.mp4?alt=media&token=5762bfad-20c2-473d-a302-0871956be2e7"
     private val url2 = "https://drive.google.com/file/d/1gBUgJKW_joIUcUcHc4P01w6IgvqZQsw4/view"
+    var fullscreen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,8 +73,49 @@ class MainActivity : AppCompatActivity() {
 
         initExoPlayer()
 
-        btn_full.setOnClickListener {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        exoplayer_fullscreen_icon.setOnClickListener {
+            if (fullscreen) {
+                exoplayer_fullscreen_icon.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        this@Fullscreen,
+                        R.drawable.ic_fullscreen_expand
+                    )
+                )
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+                if (supportActionBar != null) {
+                    supportActionBar!!.show()
+                }
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                val params =
+                    playerView.layoutParams as RelativeLayout.LayoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                params.height = (200 * applicationContext.resources
+                    .displayMetrics.density).toInt()
+                playerView.layoutParams = params
+                fullscreen = false
+                textView.setVisibility(View.VISIBLE)
+            } else {
+                exoplayer_fullscreen_icon.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        this@Fullscreen,
+                        R.drawable.ic_fullscreen_skrink
+                    )
+                )
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+                if (supportActionBar != null) {
+                    supportActionBar!!.hide()
+                }
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                val params =
+                    playerView.layoutParams as RelativeLayout.LayoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                params.height = ViewGroup.LayoutParams.MATCH_PARENT
+                playerView.layoutParams = params
+                fullscreen = true
+                textView.setVisibility(View.INVISIBLE)
+            }
         }
     }
 
